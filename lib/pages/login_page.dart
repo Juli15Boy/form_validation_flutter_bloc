96954978ed091362a2b037d94a27e3c6ed03bda5
Juli N <globaljuli@gmail.com>
2020-11-23 17:13:24 +0100

    Added Pages and first background colors

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[_createBackground(context)],
+      ),
+    );
+  }
+
+  Widget _createBackground(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(colors: <Color>[
+        Color.fromRGBO(63, 63, 156, 1.0),
+        Color.fromRGBO(90, 70, 178, 1.0),
+      ])),
+    );
+  }
+}
